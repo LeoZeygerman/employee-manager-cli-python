43e3841f99e5f19266cea81d056ffe05ea640fe5
BBYBOI <depstroug@gmail.com>
2026-05-14 23:11:24 +0500

    add first option + fixed bugs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from storage import load_data, save_data
 while True:
     try:
         print('Варианты дейтсвий: ')
@@ -7,6 +8,24 @@ while True:
         print('4. Удалить сотрудника')
         print('5. Выйти')
         choice = int(input('Выберите желаемое действие: '))
+        
+        if choice == 1:
+            data = load_data()
+            name = input('Введите имя сотрудника: ')
+            last_name = input('Введите фамилию сотрудника: ')
+            age = input('Введите возраст сотрудника: ')
+            post = input('Введите должность сотрудника: ')
+            salary = int(input('Введите зарплату сотрудника: '))
+            worker = {
+                name: 'name',
+                last_name: 'last_name',
+                age: 'age',
+                post: 'post',
+                salary: 'salary'
+            }
+            data.append(worker)
+            save_data(data)
+        
     except ValueError:
-        print ('Такого варианта ответа нет.')
+        print ('Произошла ошибка! Проверьте данные, которые вводите.')
         
--- storage.py
+++ storage.py
@@ -2,12 +2,12 @@ import json
 
 def load_data():
     try:
-        with open('workers.json', 'r') as f:
+        with open('data/workers.json', 'r') as f:
             return json.load(f)
         
     except:
         return []
     
 def save_data(data):
-    with open ('workers.json', 'w') as f:
+    with open ('data/workers.json', 'w') as f:
         json.dump(data, f)
